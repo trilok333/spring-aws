bf2bc5bb8fd16c63c008010a12fdd94dbd1b419b
Trilok reddy guntuka <trilok.guntuka@gmail.com>
2018-06-19 17:26:55 -0400

    initial commit

---
+++ src/main/java/com/common/springaws/SpringAwsApplication.java
@@ -0,0 +1,12 @@
+package com.common.springaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAwsApplication.class, args);
+	}
+}
